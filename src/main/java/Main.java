a74c5d62b706be30126d1689d5239a8c49c5cd50
leo <44844535+leoxyu@users.noreply.github.com>
2022-01-16 11:37:42 -0800

    created basic UI -> popup for file selection, timestamps auto open

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.stage.FileChooser;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -15,12 +16,19 @@ public class Main {
 
     // shouldn't write code here until other classes are complete
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the name of the video. (with file extension)");
-        String path = scanner.nextLine();
+        JFileChooser j = new JFileChooser("./././InputFolder/");
+        j.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
-        String input = "./././InputFolder/" + path;
+        j.showSaveDialog(null);
+        // Open the save dialog
+
+        File file = j.getSelectedFile();
+        String fileName = file.getName();
+
+        //System.out.println(fileName);
+
+        String input = "./././InputFolder/" + fileName;
         String output = "././././temp.mp4";
 
         double videoLength = VideoCompressor.compress(input, output) / 1000000;
@@ -28,18 +36,32 @@ public class Main {
             System.exit(0);
         }
 
-
         VideoConverter videoConverter = new VideoConverter();
-        if (videoConverter.inputVideo(output, imageArray)) {
+        if (!videoConverter.inputVideo(output, imageArray)) {
             System.exit(0);
         }
 
         double secondsPerFrame = videoLength / imageArray.size();
         timestampArray = FrameCompare.timeStamp(imageArray, secondsPerFrame);
         OutputHandler.writeFile(timestampArray);
-        System.out.println("Timestamps written to Timestamps.txt");
+        System.out.println("Timestamps written to timestamps.txt");
 
-        new File(output).delete();
+        //try to open file
+        try {
+            File timestamps = new File("././././timestamps.txt");
+            Desktop desktop = Desktop.getDesktop();
+            if(timestamps.exists()){
+                desktop.open(timestamps);
+            }
+        }catch (Exception e){
+            System.out.println("Could not automatically open timestamps.txt");
+        }
+
+        if(new File(output).delete()){
+
+        }else{
+
+        }
     }
 
 
